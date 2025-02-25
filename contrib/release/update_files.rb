4dec2795194506585023e5869142743fc9cec2e7
Martin Gruner <mg@zammad.com>
2025-02-25 11:36:51 +0100

    Maintenance: Add new breaking changes document

--- contrib/release/update_files.rb
+++ contrib/release/update_files.rb
@@ -20,6 +20,7 @@ changelog = <<~CHANGELOG
   # Change Log for Zammad #{new_version}
 
   - [Release notes](https://zammad.com/en/releases/#{new_version.tr('.', '-').sub(%r{-0$}, '')})
+  - [Breaking changes](BREAKING_CHANGES.md##{new_version_major}.#{new_version_minor})
   - [Implemented enhancements](https://github.com/zammad/zammad/issues?q=is%3Aclosed+milestone%3A#{new_version_major}.#{new_version_minor}+(-type%3ABug+AND+-label%3Abug))
   - [Closed bugs](https://github.com/zammad/zammad/issues?q=is%3Aclosed+milestone%3A#{new_version_major}.#{new_version_minor}+(type%3ABug+OR+label%3Abug))
   - [Full commit log](https://github.com/zammad/zammad/compare/#{previous_stable_version}...#{new_version})
