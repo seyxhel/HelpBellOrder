04ec9d8ea0661934a8975e69cbad269adc26d069
Dominik Klein <dk@zammad.com>
2025-03-10 23:03:57 +0100

    Maintenance: Desktop view - Fix problem that cached search count was never updated.

--- app/frontend/shared/server/apollo/cache/utils/registerOffsetStylePagination.ts
+++ app/frontend/shared/server/apollo/cache/utils/registerOffsetStylePagination.ts
@@ -31,7 +31,7 @@ const offsetLimitPagination = (
       }
 
       return {
-        totalCount: existing?.totalCount || incoming?.totalCount || 0,
+        totalCount: incoming?.totalCount || existing?.totalCount || 0,
         items: mergedItems,
       }
     },
