437e56969010b601f3d2a9bae076ba9c5fbf4d32
Benjamin Scharf <bs@zammad.com>
2025-04-07 13:34:23 +0200

    Maintenance: Desktop view - Add test for useReactivate

--- app/frontend/apps/desktop/composables/__tests__/useReactivate.spec.ts
+++ app/frontend/apps/desktop/composables/__tests__/useReactivate.spec.ts
@@ -2,9 +2,44 @@
 
 // import { useReactivate } from '#desktop/composables/useReactivate.ts'
 
+import { onMounted, ref } from 'vue'
+
+import renderComponent from '#tests/support/components/renderComponent.ts'
+import { waitForNextTick } from '#tests/support/utils.ts'
+
+import { useReactivate } from '#desktop/composables/useReactivate.ts'
+
 describe('useReactivate', () => {
-  it.todo('test useReactivate', () => {
-    // const composable = useReactivate()
-    // expect(composable)
+  it('should call callbacks appropriate', () => {
+    renderComponent({
+      template: `
+    <KeepAlive>
+       <div v-if="show"/>
+    </KeepAlive>`,
+      setup() {
+        const onActivatedCallback = vi.fn()
+        const onDeactivatedCallback = vi.fn()
+
+        const show = ref(true)
+
+        useReactivate(onActivatedCallback, onDeactivatedCallback)
+
+        onMounted(() => {
+          // Initial mounting component should not call the callback
+          expect(onActivatedCallback).not.toHaveBeenCalled()
+
+          setTimeout(async () => {
+            show.value = false
+            await waitForNextTick()
+            expect(onDeactivatedCallback).toHaveBeenCalled()
+            show.value = true
+            await waitForNextTick()
+            expect(onActivatedCallback).toHaveBeenCalled()
+          }, 50)
+        })
+
+        return { show }
+      },
+    })
   })
 })
